b6249cf7982262d3a9d6f490d2cb878a88268550
hamza essafar <essafarhamza93@gmail.com>
2024-05-01 09:59:06 +0000

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+
+ import { LampDemo  } from "./lamp";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main  >
+       
+      <LampDemo/>
+    </main>
+  );
+}
